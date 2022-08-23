7c38e99c48caac73fc23c0f56edaf480b8f7a0a3
lazy <1776266649@qq.com>
2022-08-23 22:31:22 +0800

    day02

---
+++ service/service-product/src/main/java/com/atguigu/gmall/product/controller/AttrController.java
@@ -0,0 +1,66 @@
+package com.atguigu.gmall.product.controller;
+
+import com.atguigu.gmall.common.result.Result;
+import com.atguigu.gmall.model.product.BaseAttrInfo;
+import com.atguigu.gmall.model.product.BaseAttrValue;
+import com.atguigu.gmall.product.service.BaseAttrInfoService;
+import com.atguigu.gmall.product.service.BaseAttrValueService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @Author: LAZY
+ * @Date: 2022/08/23/2022/8/23
+ */
+
+@RequestMapping("/admin/product") //抽取公共路径
+@RestController
+public class AttrController {
+
+    @Autowired
+    BaseAttrInfoService baseAttrInfoService;
+
+    @Autowired
+    BaseAttrValueService baseAttrValueService;
+
+    /**
+     * 根据分类id获取平台属性
+     * @param category1Id
+     * @param category2Id
+     * @param category3Id
+     * @return
+     */
+    @GetMapping("/attrInfoList/{category1Id}/{category2Id}/{category3Id}")
+    public Result attrInfoList(@PathVariable("category1Id")Long category1Id,
+                               @PathVariable("category2Id")Long category2Id,
+                               @PathVariable("category3Id")Long category3Id){
+        List<BaseAttrInfo> attrInfoList= baseAttrInfoService.attrInfoList(category1Id,category2Id,category3Id);
+        return Result.ok(attrInfoList);
+    }
+
+    /**
+     * 根据平台属性ID获取平台属性对象数据
+     * @param attrId
+     * @return
+     */
+    @GetMapping("/getAttrValueList/{attrId}")
+    public Result getAttrValueList(@PathVariable("attrId") Long attrId){
+        List<BaseAttrValue> baseAttrValueList = baseAttrValueService.getAttrValueList(attrId);
+        return Result.ok(baseAttrValueList);
+    }
+
+    /**
+     * 添加平台属性 / 修改平台属性
+     * @return
+     */
+    @PostMapping("/saveAttrInfo")
+    public Result saveAttrInfo(@RequestBody BaseAttrInfo baseAttrInfo){
+        baseAttrInfoService.saveAttrInfo(baseAttrInfo);
+        return Result.ok();
+    }
+
+
+
+}
